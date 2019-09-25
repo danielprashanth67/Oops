d05bd84bd404d921fdac5125e133f6383f6d7c1f
daniel <naguri@mail.usf.edu>
2019-09-24 21:13:09 -0400

    Add project files.

---
+++ Oops/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oops
+{
+    class Employee:Person
+    {
+        int Salary;
+        Employee(int sal)
+        {
+            String FullName = FirstName + LastName;
+            sal = Salary;
+            Console.WriteLine("Employee " + FullName + "Salary is" + sal.ToString());
+        }
+        Employee()
+        {
+            String FullName = FirstName + LastName;
+            Salary = 50;
+            Console.WriteLine("Employee " + FullName + "Salary is" + Salary);
+        }
+    }
+}
---
+++ Oops/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oops
+{
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public Person()
+        {
+            FirstName = "Daniel";
+            LastName = "Naguri";
+            string FullName = FirstName + LastName;
+                }
+        public Person(string FName,string LName)
+        {
+            FirstName = FName;
+            LastName = LName;
+            string FullName = FirstName + LastName;
+        }
+    }
+}
